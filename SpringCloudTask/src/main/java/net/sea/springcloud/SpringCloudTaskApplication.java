8d33e5dfe8cd58164046a2693cde0e9569648327
sea <hpw851218@163.com>
2019-03-26 15:26:59 +0800

    修改task依赖

--- SpringCloudTask/src/main/java/net/sea/springcloud/SpringCloudTaskApplication.java
+++ SpringCloudTask/src/main/java/net/sea/springcloud/SpringCloudTaskApplication.java
@@ -1,8 +1,14 @@
 package net.sea.springcloud;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 //@EnableTask
